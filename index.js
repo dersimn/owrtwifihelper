18f49435aea69cd09520aa78aba205f8a4f15681
Simon Christmann <simon@christmann.email>
2018-04-07 22:18:39 +0200

    Init

---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+const pkg = require('./package.json');
+const log = require('yalm');
+const config = require('yargs')
+    .env('OWRTWIFIHELPER')
+    .usage(pkg.name + ' ' + pkg.version + '\n' + pkg.description + '\n\nUsage: $0 [options]')
+    .describe('verbosity', 'possible values: "error", "warn", "info", "debug"')
+    .describe('name', 'instance name. used as mqtt client id and as prefix for connected topic')
+    .describe('mqtt-url', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
+    .describe('owrtwifi-prefix', 'mqtt prefix of owrtwifi')
+    .describe('owrtwifi-timeout', 'timeout for lastseen detection')
+    .alias({
+        h: 'help',
+        m: 'mqtt-url',
+        v: 'verbosity'
+    })
+    .default({
+        name: 'owrtwifihelper',
+        'mqtt-url': 'mqtt://127.0.0.1',
+        'owrtwifi-prefix': 'owrtwifi',
+        'owrtwifi-timeout': 240
+    })
+    .version()
+    .help('help')
+    .argv;
+const MqttSmarthome = require('mqtt-smarthome-connect');
+const Timer = require('yetanothertimerlibrary');
+
+log.setLevel(config.verbosity);
+log.info(pkg.name + ' ' + pkg.version + ' starting');
+log.debug("loaded config: ", config);
+
+var wifiDevices = {};
+
+log.info('mqtt trying to connect', config.mqttUrl);
+const mqtt = new MqttSmarthome(config.mqttUrl, {
+    logger: log,
+    will: {topic: config.name + '/connected', payload: '0', retain: true}
+});
+mqtt.connect();
+
+mqtt.on('connect', () => {
+    log.info('mqtt connected', config.mqttUrl);
+    mqtt.publish(config.name + '/connected', '1', {retain: true});
+});
+
+mqtt.subscribe(config.owrtwifiPrefix + '/status/+/lastseen/epoch', (topic, message, wildcard) => {
+    let mac = wildcard[0];
+
+    if (!(mac in wifiDevices)) {
+        wifiDevices.mac = {};
+        wifiDevices.mac.timer = new Timer(() => {
+            mqtt.publish(config.name + '/status/' + mac, false);
+        }, config.owrtwifiTimeout * 1000);
+    }
+
+    wifiDevices.mac.timer.reset();
+    mqtt.publish(config.name + '/status/' + mac, true);
+});
+mqtt.subscribe(config.owrtwifiPrefix + '/status/+/event', (topic, message, wildcard) => {
+    let mac = wildcard[0];
+    
+    if (!(mac in wifiDevices)) {
+        wifiDevices.mac = {};
+        wifiDevices.mac.timer = new Timer(() => {
+            mqtt.publish(config.name + '/status/' + mac, false);
+        }, config.owrtwifiTimeout * 1000);
+    }
+
+    if (message == 'new') {
+        mqtt.publish(config.name + '/status/' + mac, true);
+    }
+});
